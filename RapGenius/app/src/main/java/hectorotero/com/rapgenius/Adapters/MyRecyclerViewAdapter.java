74e2c1efcb8e4ed433a4ff0069e6fbc605791d82
Hectorrr72 <7hector2@gmail.com>
2015-04-03 10:35:09 +0200

    no message

--- RapGenius/app/src/main/java/hectorotero/com/rapgenius/Adapters/MyRecyclerViewAdapter.java
+++ RapGenius/app/src/main/java/hectorotero/com/rapgenius/Adapters/MyRecyclerViewAdapter.java
@@ -3,7 +3,6 @@ package hectorotero.com.rapgenius.Adapters;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
